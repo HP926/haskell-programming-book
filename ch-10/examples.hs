a5e8b3d3bc9678e9f0de736b8b954ff08db8e442
Hector Pina <hector.pina@acimacredit.com>
2020-10-21 20:30:04 -0600

    ch-10 10.5

---
+++ ch-10/examples.hs
@@ -0,0 +1,22 @@
+module Examples where
+
+xs :: [String]
+xs = map show [1..5]
+
+y :: [Char]
+y = foldr (\x y -> concat
+  ["(",x,"+",y,")"]) "0" xs
+
+
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny f xs =
+  foldr (\x b -> f x || b) False xs
+
+-- Prelude> myAny even [1..]
+-- True
+
+const :: a -> b -> a 
+const x _ = x
+
+--  Prelude> const 1 2
+-- 1
